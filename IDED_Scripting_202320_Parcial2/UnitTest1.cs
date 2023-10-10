aea72fe22797d558bad131825e5293119c11e7fc
Alafresh <51685896+Alafresh@users.noreply.github.com>
2023-10-09 20:21:36 -0500

    Add project files.

---
+++ IDED_Scripting_202320_Parcial2/UnitTest1.cs
@@ -0,0 +1,17 @@
+
+namespace IDED_Scripting_202320_Parcial2
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
